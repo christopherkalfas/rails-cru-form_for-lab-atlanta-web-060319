e68c8207e765e690d3becad28b1850cd2c0ed9f0
github username <github email address>
2019-09-14 10:40:38 -0400

    refresher on rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Song.destroy_all
+Artist.destroy_all
+Genre.destroy_all
+
+artists = Artist.create([{name: "Kayne West", bio: "Married Kim K"},{name: "Adele", bio:"Brit who chases streets"}])
+
+artists.each{|artist|
+Artist.create(artist)}
+
+
+genres = Genre.create([{name: "Hip-Hop"},{name: "Pop"}])
+
+genres.each {|genre|
+Genre.create(genre)}
+
+
